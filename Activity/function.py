c3cf7792ec85447a7028ade4a3df9f0d365f7625
Jeffrey Sedoro <jeffreysedoro5@gmail.com>
2024-06-14 07:28:19 +0800

    final: PythonProject

---
+++ Activity/function.py
@@ -0,0 +1,17 @@
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero" 
+    return a / b
